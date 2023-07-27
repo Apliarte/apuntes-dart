ea8f01399b4290fcd53e0cbc14c5aaf37395703c
ApliArte <erbolamm@gmail.com>
2023-07-27 10:54:48 +0200

    bool

--- Dart devtalles/ejercicios/01_basico/02_datatypes.dart
+++ Dart devtalles/ejercicios/01_basico/02_datatypes.dart
@@ -1,17 +1,59 @@
 
 main(){
+// para comentar multiples lineas se señalan estas y se pulsa crt+k+c y para descomentar pulsamos crt+k+u
 
+
+/*también se pueden
+comentar asi
+em multiples
+lineas
+ */
 
   // números
   int a=10;
   double b= 5.5;
   int? _a= 30;
   double $b=40;
+  int? c;
 
   double resultado =_a+$b;
-  print(a);
-  print(resultado);
+  // print(a);
+  // print(b);
+  // print(resultado);
+  // print(c);
+
+  // String === >
+
   
+String nombre= 'Javier';
+String apellido= 'Mateo';
+String restaurantePreferido = "Mc'Donald";
+String nombCompleto= nombre + apellido;
+String intro= '''Apliarte
+biennvenido seas titi tite tita 
+teta toto o tito''';
+
+
+// print(nombre);
+// print(apellido);
+// print(nombCompleto);
+// print(restaurantePreferido);
+// print(intro);
+
+
+// ==== Booleanos
+
+bool estaActivo =true;
+bool estaInactivo = !estaActivo;
+bool? estaNulo;
+
+
+print(estaActivo);
+print(estaInactivo);
+print(estaNulo);
+
+
+
 }
 
 
