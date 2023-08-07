5f11c817094f6867c5906a4ff58215835c96d4b8
ApliArte <erbolamm@gmail.com>
2023-08-07 20:46:24 +0200

    queue

--- Dart devtalles/ejercicios/03_funciones/04_flecha.dart
+++ Dart devtalles/ejercicios/03_funciones/04_flecha.dart
@@ -1,11 +1,26 @@
+main() {
+  int a = 10, b = 20;
 
+  int resultado = sumarFlecha(10, 20);
 
-
-
- main( ) {
-
-  
-
-
-
+//   List<int> listado=[1,2,3,4,5,6,6,7,8,9,10,10];
+// var nuevoListado = listado.where((numero) { return numero>4;});
+  List<int> listado=[1,2,3,4,5,6,6,7,8,9,10,10];
+var nuevoListado = listado.where((n) => n>4);
+  print('Imprime la lista listado ->${listado}');
+  print('Imprime los que son mayor de 4 ->${nuevoListado}');
+  print('Esta lista que sea un set (sin repetidos) ->${nuevoListado.toSet()}');
+  print('Convierte esta lista en un set -> ${nuevoListado.toSet().toList()}');
+  print('Dime cuantos elementos que tiene esta nueva lista -> ${nuevoListado.toSet().toList().length}');
+  print('Imprime el primero de la lista ${nuevoListado.toSet().toList().first}');
+  print('Imprime el ultimo de la lista ${nuevoListado.toSet().toList().last}');
+  print('Imprime que tipo de lista = ${nuevoListado.toSet().toList().length.runtimeType}');
+  print('Cambia el tipado de lista a texto = ${nuevoListado.toString()}');
+  print('Imprime que tipo de lista = ${nuevoListado.toString().runtimeType}');
 }
+
+int sumar(int x, int y) {
+  return x + y;
+}
+
+int sumarFlecha(int x, int y) => x + y;
---
+++ Dart devtalles/ejercicios/03_funciones/05_callback.dart
@@ -0,0 +1,23 @@
+main(){
+
+obtenerUsuario('100',(Map persona){
+
+
+
+
+print(persona);
+});
+
+
+}
+
+void obtenerUsuario(String id, Function callback){
+
+
+  Map usuario ={
+
+    'id': id,
+    'nombre':'Juancarlos'
+  };
+  callback(usuario);
+}
