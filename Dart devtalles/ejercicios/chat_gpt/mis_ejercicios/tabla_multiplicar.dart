30651ca3aca0bd4efe177de750c99ed9bbb04fb4
ApliArte <erbolamm@gmail.com>
2023-09-30 00:34:59 +0200

    tablas

---
+++ Dart devtalles/ejercicios/chat_gpt/mis_ejercicios/tabla_multiplicar.dart
@@ -0,0 +1,12 @@
+/* 
+* **fold(...):** Aplica la función especificada a cada elemento de la lista, 
+y devuelve el resultado acumulado.*/
+main() {
+  List<int> lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  lista.forEach((elemento) => print('6 x $elemento = ${elemento * 6}'));
+// 1
+// 2
+// 3
+// 4
+// 5
+}
